56d407b000a092add40d157f5e933b1bb3bc1836
tanaka masato <tanaka@feedforce.jp>
2015-12-21 17:00:33 +0900

    rpmbuildするときの実行ディレクトリは実行ユーザのHOMEらしい

--- rubybuild.sh
+++ rubybuild.sh
@@ -3,8 +3,8 @@ WORK_DIR=/var/tmp
 RUBY_VERSION=$(cat $WORK_DIR/rpmbuild/SOURCES/ruby-version)
 RUBY_MINOR_VER=$(echo $RUBY_VERSION | sed -e 's@\.[0-9]$@@')
 
-cd $WORK_DIR/rpmbuild/SOURCES && curl -LO https://cache.ruby-lang.org/pub/ruby/$RUBY_MINOR_VER/ruby-$RUBY_VERSION.tar.gz
+cd $HOME/rpmbuild/SOURCES && curl -LO https://cache.ruby-lang.org/pub/ruby/$RUBY_MINOR_VER/ruby-$RUBY_VERSION.tar.gz
 
-rpmbuild -ba $WORK_DIR/rpmbuild/SPECS/ruby.spec
+rpmbuild -ba $HOME/rpmbuild/SPECS/ruby.spec
 
-cp $WORK_DIR/rpmbuild/RPMS/x86_64/* /shared/
+cp $HOME/rpmbuild/RPMS/x86_64/* /shared/
