72e45521eb0461c03b686d00705c048b3e7d054f
tanaka masato <tanaka@feedforce.jp>
2015-12-21 17:11:43 +0900

    Follow semantic versioning

--- rubybuild.sh
+++ rubybuild.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-RUBY_VERSION=$(cat $HOME/rpmbuild/SOURCES/ruby-version)
-RUBY_MINOR_VER=$(echo $RUBY_VERSION | sed -e 's@\.[0-9]$@@')
+RUBY_X_Y_Z_VERSION=$(cat $HOME/rpmbuild/SOURCES/ruby-version)
+RUBY_X_Y_VERSION=$(echo $RUBY_X_Y_Z_VERSION | sed -e 's@\.[0-9]$@@')
 
-cd $HOME/rpmbuild/SOURCES && curl -LO https://cache.ruby-lang.org/pub/ruby/$RUBY_MINOR_VER/ruby-$RUBY_VERSION.tar.gz
+cd $HOME/rpmbuild/SOURCES && curl -LO https://cache.ruby-lang.org/pub/ruby/$RUBY_X_Y_VERSION/ruby-$RUBY_X_Y_Z_VERSION.tar.gz
 
 rpmbuild -ba $HOME/rpmbuild/SPECS/ruby.spec
 
