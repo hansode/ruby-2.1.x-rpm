218ec5f52c16c3e3e24a23ff666064befdcb43bf
tanaka masato <tanaka@feedforce.jp>
2015-12-21 17:09:18 +0900

    Change execution path

--- rubybuild.sh
+++ rubybuild.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-WORK_DIR=/var/tmp
-RUBY_VERSION=$(cat $WORK_DIR/rpmbuild/SOURCES/ruby-version)
+RUBY_VERSION=$(cat $HOME/rpmbuild/SOURCES/ruby-version)
 RUBY_MINOR_VER=$(echo $RUBY_VERSION | sed -e 's@\.[0-9]$@@')
 
 cd $HOME/rpmbuild/SOURCES && curl -LO https://cache.ruby-lang.org/pub/ruby/$RUBY_MINOR_VER/ruby-$RUBY_VERSION.tar.gz
