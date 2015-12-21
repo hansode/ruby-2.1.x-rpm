138df7195e50d629dd5c4f9eb7f380d86134eb8c
tanaka masato <tanaka@feedforce.jp>
2015-12-21 14:18:37 +0900

    Improve

--- rubybuild.sh
+++ rubybuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 WORK_DIR=/var/tmp
 RUBY_VERSION=$(cat ruby-version)
-RUBY_MINOR_VER=$(echo $test | sed -e "s/\([0-9]\.[0-9]\).*/\1/")
+RUBY_MINOR_VER=$(echo $RUBY_VERSION | sed -e "s@\.[0-9]$@@")
 
 cd $WORK_DIR/rpmbuild/SOURCES && curl -LO https://cache.ruby-lang.org/pub/ruby/$RUBY_MINOR_VER/ruby-$RUBY_VERSION.tar.gz
 
